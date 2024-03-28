3744b628ee285bb5ef1a6e6bd809b857e87077e4
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-03-28 04:34:41 +0200

    add UnitTest

---
+++ src/phone-validator/phone-validator.service.spec.ts
@@ -0,0 +1,88 @@
+import { PhoneValidatorService } from './phone-validator.service';
+import { PhoneValidator } from './entities/phone-validator.entity';
+
+const date = new Date('2024-03-28 00:03:20.120286');
+describe('PhoneValidatorService', () => {
+  let phoneValidatorService: PhoneValidatorService;
+  let mockPhoneValidatorRepository;
+
+  beforeEach(() => {
+    mockPhoneValidatorRepository = {
+      find: jest.fn(),
+      save: jest.fn(),
+    };
+
+    phoneValidatorService = new PhoneValidatorService(mockPhoneValidatorRepository);
+  });
+
+  describe('create', () => {
+    it('should save phone validator rule', async () => {
+      const createPhoneValidatorDto = {
+        countryName: 'TestCountry',
+        code: '+372',
+        requiredDigits: [5],
+        min: 6,
+        max: 8,
+      };
+
+      mockPhoneValidatorRepository.save.mockResolvedValueOnce(createPhoneValidatorDto);
+
+      const result = await phoneValidatorService.create(createPhoneValidatorDto);
+
+      expect(mockPhoneValidatorRepository.save).toHaveBeenCalledWith(createPhoneValidatorDto);
+      expect(result).toEqual(createPhoneValidatorDto);
+    });
+  });
+
+  describe('validate', () => {
+    it('should return false if phone number is not in valid format: missed sign "+"', async () => {
+      const result = await phoneValidatorService.validate('123456789'); // Not starting with '+'
+      expect(result).toBe(false);
+    });
+
+    it('should return false if phone number is not in valid format: consist not only + and digits: letters', async () => {
+      const result = await phoneValidatorService.validate('123456789d'); // Not starting with '+'
+      expect(result).toBe(false);
+    });
+
+    it('should return false if no rules found for country code', async () => {
+      const mockRules: PhoneValidator[] = [];
+      mockPhoneValidatorRepository.find.mockResolvedValueOnce(mockRules);
+
+      const result = await phoneValidatorService.validate('+123456789');
+      expect(result).toBe(false);
+    });
+
+    it('should return false if digit prefix is not valid', async () => {
+      const mockRules: PhoneValidator[] = [{ id: '1', countryName:"be", code: '+32', requiredDigits: [46, 56], min: 8, max: 12, created: date, updated: date }];
+      mockPhoneValidatorRepository.find.mockResolvedValueOnce(mockRules);
+
+      const result = await phoneValidatorService.validate('+32789123');
+      expect(result).toBe(false);
+    });
+
+    it('should return false if phone length is out of range: Length < min', async () => {
+      const mockRules: PhoneValidator[] = [{ id: '1', countryName:"be", code: '+32', requiredDigits: [46, 56], min: 8, max: 12, created: date, updated: date  }];
+      mockPhoneValidatorRepository.find.mockResolvedValueOnce(mockRules);
+
+      const result = await phoneValidatorService.validate('+3245678'); // Length < min
+      expect(result).toBe(false);
+    });
+
+    it('should return false if phone length is out of range: Length > max', async () => {
+      const mockRules: PhoneValidator[] = [{ id: '1', countryName:"be", code: '+32', requiredDigits: [46, 56], min: 8, max: 12, created: date, updated: date  }];
+      mockPhoneValidatorRepository.find.mockResolvedValueOnce(mockRules);
+
+      const result = await phoneValidatorService.validate('+32456789123456789'); // Length > max
+      expect(result).toBe(false);
+    });
+
+    it('should return true if phone number is valid', async () => {
+      const mockRules: PhoneValidator[] = [{ id: '1', countryName:"be", code: '+32', requiredDigits: [46,56], min: 8, max: 12, created: date, updated: date  }];
+      mockPhoneValidatorRepository.find.mockResolvedValueOnce(mockRules);
+
+      const result = await phoneValidatorService.validate('+3246578119');
+      expect(result).toBe(true);
+    });
+  });
+});
